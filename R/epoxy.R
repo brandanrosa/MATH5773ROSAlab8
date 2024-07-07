9124e62c9e523563b34807b77a6e4944abd4d750
Brandan Rosa <brandanrosa22@gmai.com>
2024-07-07 16:14:49 -0500

    Data

---
+++ R/epoxy.R
@@ -0,0 +1,14 @@
+#' EPOXY data set
+#'
+#' A data set of steel panels coated in one of four anti-corrosive epoxy systems and how long they were exposed to corrosive material.
+#'
+#' @format
+#' A data frame with 12 rows and 3 columns:
+#' \describe{
+#'   \item{EXP-TIME}{Exposure time in corrosive substance}
+#'   \item{SYSTEM}{One of four epoxy coating systems}
+#'   \item{CORRATE}{Corrosion rate (nanoamperes per square cm)}
+#'   ...
+#' }
+#' @source <https://www.amazon.com/Statistics-Engineering-Sciences-William...
+"epoxy"
